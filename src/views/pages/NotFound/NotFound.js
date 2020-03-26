28f1ba678089e3328f2e53896032b5ebf26e7897
josue valencia <josue.valrob@gmail.com>
2020-03-26 16:45:28 +0100

    asd

--- src/views/pages/NotFound/NotFound.js
+++ src/views/pages/NotFound/NotFound.js
@@ -44,7 +44,7 @@ const NotFound = () => {
             <img
               alt="Under development"
               className={classes.image}
-              src={`${process.env.PUBLIC_URL}/undraw_page_not_found_su7k.svg}
+              src={`${process.env.PUBLIC_URL}/undraw_page_not_found_su7k.svg`}
             />
           </div>
         </Grid>
